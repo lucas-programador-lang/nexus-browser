3df91cdd041aa53537c4517c18ed1fb25e207092
lucas-programador-lang <lucas.dev.programador@outlook.com>
2026-03-11 14:45:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,51 @@
 const { app, BrowserWindow } = require("electron")
+const path = require("path")
+
+// carregar bloqueador de anúncios
+const enableAdBlock = require("./security/adblock")
+
+let mainWindow
 
 function createWindow(){
 
-const win = new BrowserWindow({
+mainWindow = new BrowserWindow({
+
 width:1400,
 height:900,
+
 webPreferences:{
-preload: __dirname + "/preload.js"
+preload: path.join(__dirname, "preload.js"),
+nodeIntegration: false,
+contextIsolation: true
+}
+
+})
+
+mainWindow.loadFile("index.html")
+
+}
+
+// quando o Electron estiver pronto
+app.whenReady().then(() => {
+
+enableAdBlock()
+
+createWindow()
+
+// recriar janela se clicar no dock (Mac)
+app.on("activate", () => {
+if(BrowserWindow.getAllWindows().length === 0){
+createWindow()
 }
 })
 
-win.loadFile("index.html")
+})
 
+// fechar app quando todas janelas fecharem
+app.on("window-all-closed", () => {
+
+if(process.platform !== "darwin"){
+app.quit()
 }
 
-app.whenReady().then(createWindow)
+})
