04bab596916ac1c76731ca5c973923379154263c
lucas-programador-lang <lucas.dev.programador@outlook.com>
2026-03-11 14:36:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const { app, BrowserWindow } = require("electron")
 
+function createWindow(){
+
+const win = new BrowserWindow({
+width:1400,
+height:900,
+webPreferences:{
+preload: __dirname + "/preload.js"
+}
+})
+
+win.loadFile("index.html")
+
+}
+
+app.whenReady().then(createWindow)
