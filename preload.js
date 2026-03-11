a2d9e798d5319733bded0be21f849719b4c80715
lucas-programador-lang <lucas.dev.programador@outlook.com>
2026-03-11 16:21:36 -0400

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,31 @@
+const { contextBridge, ipcRenderer } = require("electron")
+
+// ===============================
+// INICIAR NAVEGADOR
+// ===============================
+
 window.addEventListener("DOMContentLoaded", () => {
 
-console.log("Nexus Browser iniciado")
+console.log("🚀 Nexus Browser iniciado")
+
+})
+
+
+// ===============================
+// API SEGURA PARA FRONTEND
+// ===============================
+
+contextBridge.exposeInMainWorld("nexusAPI", {
+
+
+// enviar mensagem para o processo principal
+send: (channel, data) => {
+ipcRenderer.send(channel, data)
+},
+
+// receber mensagem
+receive: (channel, func) => {
+ipcRenderer.on(channel, (event, ...args) => func(...args))
+}
 
 })
