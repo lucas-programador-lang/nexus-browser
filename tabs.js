2f15057d99f3008cf07a6119dd74935dcc99a38e
lucas-programador-lang <lucas.dev.programador@outlook.com>
2026-03-11 14:53:09 -0400

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -1,5 +1,58 @@
-function novaAba(){
+let abas = []
+let abaAtual = null
 
-document.getElementById("browser").src = "https://google.com"
+function novaAba(url = "https://google.com"){
+
+const browserContainer = document.getElementById("browser-container")
+const tabsContainer = document.getElementById("tabs")
+
+const id = "tab-" + Date.now()
+
+// criar botão da aba
+const tabButton = document.createElement("button")
+tabButton.innerText = "Nova Aba"
+tabButton.id = id
+
+tabButton.onclick = () => trocarAba(id)
+
+tabsContainer.appendChild(tabButton)
+
+// criar navegador
+const webview = document.createElement("webview")
+
+webview.src = url
+webview.style.width = "100%"
+webview.style.height = "100%"
+webview.style.display = "none"
+
+webview.id = "view-" + id
+
+browserContainer.appendChild(webview)
+
+abas.push({
+id:id,
+webview:webview
+})
+
+trocarAba(id)
+
+}
+
+function trocarAba(id){
+
+abas.forEach(tab=>{
+
+tab.webview.style.display = "none"
+
+})
+
+const aba = abas.find(t=>t.id === id)
+
+if(aba){
+
+aba.webview.style.display = "flex"
+abaAtual = aba
+
+}
 
 }
